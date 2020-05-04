0c401e77b0308dfd3dc00f7f95ee51e4e65f1a27
eunGyeolLee <leepro225@gmail.com>
2020-05-05 01:32:05 +0900

    tab and pagecontrol

---
+++ Tab2/Tab2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tab2
+//
+//  Created by hrobotics on 2020/05/05.
+//  Copyright © 2020 eunGyeolLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+}
+
