8789be35736953384200e7ff7bc5049f05f02131
eunGyeolLee <leepro225@gmail.com>
2020-06-29 08:55:48 +0900

    20200629

--- Navigation/Navigation/EditViewController.swift
+++ Navigation/Navigation/EditViewController.swift
@@ -10,31 +10,43 @@ import UIKit
 
 protocol EditDelegate {
     func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn : Bool)
 }
 
 class EditViewController: UIViewController {
     var textWayValue :String = ""
     var textMessage: String = ""
     var delegate: EditDelegate?
-
-    @IBOutlet var lblway: UILabel!
+    var isOn = false
     
+    @IBOutlet var lblway: UILabel!
     @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swIsOn: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         lblway.text = textWayValue
         txMessage.text = textMessage
+        swIsOn.isOn = isOn
     }
     
     @IBAction func btnDone(_ sender: UIButton) {
         if delegate != nil {
             delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
         }
         _ = navigationController?.popViewController(animated: true)
     }
     
+    @IBAction func swImgOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        }
+    }
     /*
     // MARK: - Navigation
 
